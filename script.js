dce519295b168777211857d55c0896048f3505ee
Claire Utrecht <utrechtc@gmail.com>
2022-01-26 20:40:56 -0500

    pushing this so you the team can take a look at what's going on so far!

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+
+
+
+
+
+
+
+
+/* This is for the navigation bar*/
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
